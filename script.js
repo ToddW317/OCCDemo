f1ad1d436fd972776a741fba83a7f884b4fe2e76
ToddW317 <wooten00@gmail.com>
2023-08-05 04:46:45 -0700

    basic functions implemented

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
-const container = document.querySelector(".container");
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
 
-document.querySelector(".open-navbar-icon").addEventListener("click", () => {
-  container.classList.add("change");
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
+
+$('.toggle-menu').click (function(){
+	$(this).toggleClass('active');
+	$('#menu').toggleClass('open');
+  });
+
+
+  const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
 });
 
-document.querySelector(".close-navbar-icon").addEventListener("click", () => {
-  container.classList.remove("change");
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
 });
 
-const colors = ["#ADADC3", "#70707A", "#ADADC3", "#f08080", "#afeeee"];
 
-let i = 0;
+//Slide in on scroll
+const hiddenElements = document.querySelectorAll('.hidden')
 
-Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
-  item.style.cssText = `background-color: ${colors[i++]}`;
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} else {
+			entry.target.classList.remove('show');
+		}
+	});
 });
 
-Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
-  item.onclick = () => {
-    item.parentElement.parentElement.classList.toggle("change");
-  };
-});
+
+hiddenElements.forEach((el) => observer.observe(el));
